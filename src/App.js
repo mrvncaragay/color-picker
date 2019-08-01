6cefa9cbe0f723428d90c1791758be38f0020a59
Marvin Caragay <mrvncaragay@gmail.com>
2019-08-01 13:10:21 -0700

    added react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 
 function App() {
-  return <Palette palette={generatePalette(seedColors[4])} />;
+  return (
+    <Switch>
+      <Route exact path="/" render={() => <h1>Index Route</h1>} />
+      <Route
+        exact
+        path="/palette/:id"
+        render={() => <h1>Individual Route</h1>}
+      />
+    </Switch>
+
+    //{/* <Palette palette={generatePalette(seedColors[4])} />; */}
+  );
 }
 
 export default App;
