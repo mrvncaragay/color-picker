672e35e670c3dd33d4e63566801e81db920c58f7
Marvin Caragay <mrvncaragay@gmail.com>
2019-08-01 14:01:21 -0700

    added material ui HOC

---
+++ src/MiniPalette.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { withStyles } from "@material-ui/styles";
+import { whileStatement } from "@babel/types";
+
+const styles = {
+  main: {
+    backgroundColor: "purple",
+    border: "3px solid teal",
+    "& h1": {
+      color: "white"
+    }
+  }
+};
+
+const MiniPalette = props => {
+  const { classes } = props;
+  return (
+    <div className={classes.main}>
+      <h1>Mini Palette</h1>
+    </div>
+  );
+};
+
+export default withStyles(styles)(MiniPalette);
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import MiniPalalette from "./MiniPalette";
 
 const PaletteList = ({ palettes }) => {
   return (
     <div>
+      <MiniPalalette />
       {palettes.map(palette => (
         <p>
           <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
