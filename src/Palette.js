e7b9f4135547aa8f657d50d1655fc8c50efdb669
Marvin Caragay <mrvncaragay@gmail.com>
2019-08-01 11:23:59 -0700

    styling the color slider

--- src/Palette.js
+++ src/Palette.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import ColorBox from "./ColorBox";
-import "./Palette.css";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
+import "./Palette.css";
 
 const Palette = ({ palette }) => {
   const [level, setLevel] = useState(500);
@@ -17,13 +17,16 @@ const Palette = ({ palette }) => {
 
   return (
     <div className="Palette">
-      <Slider
-        defaultValue={level}
-        min={100}
-        max={900}
-        onAfterChange={changeLevel}
-        step={100}
-      />
+      <div className="slider">
+        <Slider
+          defaultValue={level}
+          min={100}
+          max={900}
+          onAfterChange={changeLevel}
+          step={100}
+        />
+      </div>
+
       {/* Navbar */}
 
       <div className="Palette-colors">{colorBoxes}</div>
