7ce515ffb1456c9c9cdfa2abfb1f3a275963c092
Marvin Caragay <mrvncaragay@gmail.com>
2019-08-08 20:13:02 -0700

    make color box responsive

--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -15,15 +15,15 @@ export default {
     },
     [sizes.down("lg")]: {
       width: "25%",
-      height: props => (props.showingFullPalette ? "20%" : "50%")
+      height: props => (props.showingFullPalette ? "20%" : "33.333%")
     },
     [sizes.down("md")]: {
       width: "50%",
-      height: props => (props.showingFullPalette ? "10%" : "50%")
+      height: props => (props.showingFullPalette ? "10%" : "20%")
     },
     [sizes.down("xs")]: {
       width: "100%",
-      height: props => (props.showingFullPalette ? "5%" : "50%")
+      height: props => (props.showingFullPalette ? "5%" : "10%")
     }
   },
   copyText: {
@@ -127,7 +127,10 @@ export default {
       textAlign: "center",
       marginBottom: 0,
       padding: "1rem",
-      textTransform: "uppercase"
+      textTransform: "uppercase",
+      [sizes.down("xs")]: {
+        fontSize: "6rem"
+      }
     },
 
     "& p": {
